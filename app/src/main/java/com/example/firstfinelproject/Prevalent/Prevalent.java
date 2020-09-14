590098784b3ee50b803a75f8cd0538a5aca11155
Abdalrahman J Shaban <37397337+abood-sh@users.noreply.github.com>
2020-09-14 13:25:29 +0300

    E-commerce

---
+++ app/src/main/java/com/example/firstfinelproject/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.example.firstfinelproject.Prevalent;
+
+import com.example.firstfinelproject.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
